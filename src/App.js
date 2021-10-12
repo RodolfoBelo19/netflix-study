fc3c3718077b2189578946563eca264c0fd19695
Rodolfo Belo <rodolfobelo@MacBook-Air-de-Rodolfo.local>
2021-10-11 23:29:23 -0300

    feat: MovieRow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
+
+    const [movieList, setMovieList] = useState([]);
+
 
     useEffect(() => {
         const loadAll = async () => {
             let list = await Tmdb.getHomeList();
-            console.log(list)
+            setMovieList(list);
         }
 
         loadAll()
@@ -14,7 +18,13 @@ export default () => {
     }, []);
 
     return (
-        <div>Teste </div>
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
+        </div>
     )
 
     
